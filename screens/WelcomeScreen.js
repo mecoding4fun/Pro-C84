7a19e8649ddf5898daec0375683d84bb68277c27
Darknessoft <53414760+mecoding4fun@users.noreply.github.com>
2020-08-04 17:01:48 +0530

    Update WelcomeScreen.js

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Text, Image, TouchableOpacity,TextInput, Alert, Modal,ScrollView,KeyboardAvoidingView } from 'react-native';
-import BarterAnimation from '../components/BarterAnimationScreen.js';
 
 import db from '../config';
 import firebase from 'firebase';
